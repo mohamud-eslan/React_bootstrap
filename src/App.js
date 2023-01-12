5dbd2cba9f42721211735d4e0b6ebcdc83fb1a54
mohamud <mohmud141@gmail.com>
2023-01-12 20:10:18 +0300

    an other commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
  import 'bootstrap/dist/css/bootstrap.min.css';
  import { Button, form } from 'react-bootstrap';
 import InfoForm from './Component.js/InfoForm';
+import TaskTracker from './Component.js/TaskTracker';
 
 
 function App() {
+
   return (
-    <div className="App">
-      <InfoForm />   
+    <div className="container">
+      <TaskTracker />
+      {/* <InfoForm /> */}
+        
     </div>
   );
 }
---
+++ src/Component.js/TaskTracker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TaskTracker = () => {
+    const name = 'mohamud'
+  return (
+    <header>
+         <h1>hello {name}</h1>
+    </header>
+  )
+}
+
+export default TaskTracker
