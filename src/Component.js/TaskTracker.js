14fb294fcac96fa393d353f16c933b8e7024f6ab
mohamud <mohmud141@gmail.com>
2023-01-12 20:17:59 +0300

    an other

--- src/Component.js/TaskTracker.js
+++ src/Component.js/TaskTracker.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const TaskTracker = () => {
-    const name = 'mohamud'
+    const name = 'mohamud';
+    const age = 34
   return (
     <header>
-         <h1>hello {name}</h1>
+         <h1>hello {name} and my age is {age}</h1>
     </header>
   )
 }
