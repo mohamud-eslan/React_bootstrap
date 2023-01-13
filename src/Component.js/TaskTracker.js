d8234a52f3219a4fa8cd9008c217a1601e7a881b
mohamud <mohmud141@gmail.com>
2023-01-13 11:50:50 +0300

    count

--- src/Component.js/TaskTracker.js
+++ src/Component.js/TaskTracker.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const TaskTracker = () => {
-    const name = 'mohamud';
+  const [count, setCount] = useState(0)
+  const handleClick = () =>{
+    setCount((preCount) =>preCount + 1)
+  }
     const age = 34
   return (
     <header>
-         <h1>hello {name} and my age is {age}</h1>
+         <h1>hello {count} and my age is {age}</h1>
+         <button onClick={handleClick}>clic</button>
     </header>
   )
 }
