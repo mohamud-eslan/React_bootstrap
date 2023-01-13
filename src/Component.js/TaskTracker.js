9b4909425e5ae642561e0ed0b1143af6e596328b
mohamud <mohmud141@gmail.com>
2023-01-13 11:54:17 +0300

    setcount

--- src/Component.js/TaskTracker.js
+++ src/Component.js/TaskTracker.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 
 const TaskTracker = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(4)
   const handleClick = () =>{
     setCount((preCount) =>preCount + 1)
   }
     const age = 34
   return (
     <header>
-         <h1>hello {count} and my age is {age}</h1>
+         <h1>number {count} and my age is {age}</h1>
          <button onClick={handleClick}>clic</button>
     </header>
   )
