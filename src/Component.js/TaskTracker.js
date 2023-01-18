c2cd3b2ed91de75b2cc038f71409d74f4aa4597c
mohamud <mohmud141@gmail.com>
2023-01-18 11:35:10 +0300

    test

--- src/Component.js/TaskTracker.js
+++ src/Component.js/TaskTracker.js
@@ -1,23 +1,19 @@
 import React, {useState} from 'react'
 
 const TaskTracker = () => {
-  const [count, setCount] = useState(4)
-  const [age, setAge] = useState(20)
-  
-  const handleClick = () =>{
-    setCount((preCount) =>preCount + 1)
-  }
-    const changeAge = ()=>{
-      setAge((preAge) => preAge + 2)
-    }
-    const name = 'mohamud'
+  const name = 'ahmed'
   return (
-    <header>
-        <h1 className='tail' bg-red>hi {name}</h1>
-         <h1>number {count} and my age is {age}</h1>
-         <button onClick={handleClick}>clic</button>
-         <button onClick={changeAge}>click</button>
-    </header>
+    <div>
+        <h1>hi {4 + 3}</h1>
+        {name ? (
+          <>
+          test
+          </>
+        ):
+        (
+          test
+        )}
+    </div>
   )
 }
 
